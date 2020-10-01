90e8d3abf27fed32b3235876d15c36c091c442a4
vbueno <viniciusbueno15@gmail.com>
2020-10-01 17:15:14 -0300

    initial configurations

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator />,
+  </div>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+  document.getElementById('root'));
 serviceWorker.unregister();
